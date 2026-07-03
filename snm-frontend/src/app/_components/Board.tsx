af0a62f2095582031e224363d8a1f61a3e75b63b
Arnav <44arnavmishra@gmail.com>
2026-07-03 18:43:09 +0530

    Minor formatting changes and community cards and player hand states

--- snm-frontend/src/app/_components/Board.tsx
+++ snm-frontend/src/app/_components/Board.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import Image from "next/image";
 import React, { use, useEffect, useState } from "react";
 import { ConnectionContext } from "@/lib/context";
-import { Page, PlayerData } from "@/lib/types";
+import { Card, Page, PlayerData } from "@/lib/types";
 import Button from "./Button";
 
 export const Table = styled.div`
@@ -29,7 +29,7 @@ export const Hand = styled.div`
     background-color: white;
 `
 
-export function CardHand()
+export function OtherPlayerHand()
 {
     return(
         <div
@@ -58,13 +58,32 @@ export function CardHand()
     );
 }
 
+export function SelfHand({cards} : {cards: Card[]})
+{
+    return(
+        <div className="relative flex flex-col justify-center items-center w-60 h-60 aspect-square">
+            <Image
+            className="absolute w-40 h-56 -rotate-10 -translate-x-10"
+            src={`/cards/ace_spades.svg`}
+            width={0} height={0}
+            alt="First hand card" />
+
+            <Image
+            className="absolute w-40 h-56 rotate-10 translate-x-10"
+            src={`/cards/ace_spades.svg`}
+            width={0} height={0}
+            alt="Second hand card" />
+        </div>
+    );
+}
+
 export function HandShaft({ rotation, name } : {rotation: number, name: string})
 {
     return(
         <div
         className="absolute flex justify-end w-full"
         style={{rotate: `${rotation}deg`}}>
-            <CardHand />
+            <OtherPlayerHand />
             <span
             className="fixed p-2 rounded-xl translate-x-15 text-2xl font-bold bg-gray-900/80"
             style={{rotate: `${-rotation}deg`}}>
@@ -83,6 +102,13 @@ export default function Board()
     const [hostUID, SetHostUID] = useState<string>("");
     const [players, SetPlayersValue] = useState<PlayerData>({});
     const [gameStarted, SetGameStarted] = useState<boolean>(false);
+    const [communityCards, SetCommunityCards] = useState<Card[]>([]);
+    const [playerHand, SetPlayerHand] = useState<Card[]>([]);
+
+    function SendStartMessage()
+    {
+        SetGameStarted(true);
+    }
 
     useEffect(() => {
         const ws = context.roomCode
@@ -107,6 +133,15 @@ export default function Board()
                     break;
                 case "new-host":
                     SetHostUID(parsedData.Payload);
+                    break;
+                case "start-game":
+                    SetGameStarted(true);
+                    break;
+                case "card-hand":
+                    SetPlayerHand(parsedData.Payload);
+                    break;
+                case "community-cards":
+                    SetCommunityCards(parsedData.Payload);
                     break;
             }
         };
@@ -138,7 +173,7 @@ export default function Board()
     });
 
     return(
-        <div className="fixed flex flex-col justify-center items-center w-dvw h-dvh gap-5">
+        <div className="fixed flex flex-col justify-center items-center w-dvw h-dvh gap-3">
             { gameStarted
             ?
             <span className="text-6xl p-5">00:00</span>
@@ -149,26 +184,20 @@ export default function Board()
             
             { gameStarted
             ?
-                <div className="relative flex justify-center items-center w-70 h-75">
-                    <Image
-                    className="absolute w-40 h-56 -rotate-10 -translate-x-10"
-                    src={`/cards/ace_spades.svg`}
-                    width={0} height={0}
-                    alt="First hand card" />
-
-                    <Image
-                    className="absolute w-40 h-56 rotate-10 translate-x-10"
-                    src={`/cards/ace_spades.svg`}
-                    width={0} height={0}
-                    alt="Second hand card" />
+            <div className="flex flex-row flex-wrap justify-center items-center gap-2">
+                <SelfHand cards={playerHand} />
+                <div className="py-5 px-3 text-6xl rounded-xl bg-black/10">
+                    <span>Role: </span>
+                    <span className="border-2 border-white rounded-2xl p-2">None</span>
                 </div>
+            </div>
             :
                 <div className="flex flex-col w-dvw items-center gap-15">
                     <div className="text-5xl">Room code: <span className="leading-none p-3 rounded-xl bg-black/10">{context.roomCode}</span></div>
                     {
                     selfUID === hostUID
-                    ? <Button onClick={() => SetGameStarted(true)}>Start Game</Button>
-                    : <span>Waiting for host to start the game...</span>
+                    ? <Button onClick={SendStartMessage}>Start Game</Button>
+                    : <span className="text-xl">Waiting for host to start the game...</span>
                     }
                 </div> }
         </div>
